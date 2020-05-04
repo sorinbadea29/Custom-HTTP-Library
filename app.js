f332962b957b14cb829574acb62d73e70c3bd1ec
Sorin Badea <62335902+sorinbadea29@users.noreply.github.com>
2020-05-04 11:39:53 +0300

    Update app.js

--- app.js
+++ app.js
@@ -12,6 +12,15 @@ http.get('https://jsonplaceholder.typicode.com/posts', function(res, err){
 });
 
 
+// http.get('https://jsonplaceholder.typicode.com/posts/1', function(err, res){
+//   if(err){
+//     console.log(err)
+//   }else{
+//     console.log(res)
+//   }
+// });
+
+
 // http.post('https://jsonplaceholder.typicode.com/posts', postData,  function(res, err){
 //   if(res){
 //     console.log(res);
